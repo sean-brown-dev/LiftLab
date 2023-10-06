7b241e82c0d957d0c248baeb4376df682aa93f03
Sean Brown <seanbrown91@gmx.com>
2023-10-05 23:02:31 -0400

    more charting

--- app/src/main/java/com/browntowndev/liftlab/ui/views/main/liftlibrary/liftdetails/ChartsTab.kt
+++ app/src/main/java/com/browntowndev/liftlab/ui/views/main/liftlibrary/liftdetails/ChartsTab.kt
@@ -1,5 +1,6 @@
 package com.browntowndev.liftlab.ui.views.main.liftlibrary.liftdetails
 
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
@@ -10,28 +11,43 @@ import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
 import com.patrykandpatrick.vico.compose.chart.Chart
 import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.compose.chart.scroll.rememberChartScrollSpec
+import com.patrykandpatrick.vico.compose.chart.scroll.rememberChartScrollState
 import com.patrykandpatrick.vico.compose.m3.style.m3ChartStyle
 import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
 import com.patrykandpatrick.vico.core.axis.AxisPosition
 import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.chart.scale.AutoScaleUp
 import com.patrykandpatrick.vico.core.entry.ChartEntryModel
+import com.patrykandpatrick.vico.core.scroll.AutoScrollCondition
+import com.patrykandpatrick.vico.core.scroll.InitialScroll
 import kotlin.math.roundToInt
 
 @Composable
 fun ChartsTab(
     oneRepMaxChartValues: Pair<ChartEntryModel, AxisValueFormatter<AxisPosition.Horizontal.Bottom>>,
 ) {
+
+    val scrollState = rememberChartScrollState()
+    val scrollSpec = rememberChartScrollSpec( isScrollEnabled = true ,
+        autoScrollAnimationSpec = tween(0) ,
+        autoScrollCondition = AutoScrollCondition.OnModelSizeIncreased,
+        initialScroll = InitialScroll.End )
+
     Card (modifier = Modifier.padding(10.dp)) {
         ProvideChartStyle(m3ChartStyle()) {
             Chart(
                 modifier = Modifier.height(250.dp),
-                chart = lineChart(),
+                chart = lineChart(spacing = 75.dp),
                 model = oneRepMaxChartValues.first,
                 startAxis = rememberStartAxis(valueFormatter = { value, _ ->
                     value.roundToInt().toString()
                 }),
                 bottomAxis = rememberBottomAxis(valueFormatter = oneRepMaxChartValues.second),
                 marker = rememberMarker(),
+                autoScaleUp = AutoScaleUp.Full,
+                chartScrollState = scrollState,
+                chartScrollSpec = scrollSpec,
             )
         }
     }
