39ae15e12a877767a07664466a31774b24ce7ef5
Sean Brown <seanbrown91@gmx.com>
2023-11-20 16:13:19 -0500

    consistent chart names

--- app/src/main/java/com/browntowndev/liftlab/ui/views/main/liftlibrary/liftdetails/ChartsTab.kt
+++ app/src/main/java/com/browntowndev/liftlab/ui/views/main/liftlibrary/liftdetails/ChartsTab.kt
@@ -9,6 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.browntowndev.liftlab.core.common.enums.LiftMetricChartType
+import com.browntowndev.liftlab.core.common.enums.displayName
 import com.browntowndev.liftlab.ui.models.ChartModel
 import com.browntowndev.liftlab.ui.models.ComposedChartModel
 
@@ -32,21 +34,28 @@ fun ChartsTab(
         verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
         SingleLineWorkoutFilterableChart(
-            label = remember(oneRepMaxChartModel) { "ESTIMATED ONE REP MAX ${if(oneRepMaxChartModel?.hasData != true) "- NO DATA" else ""}" },
+            label = remember(oneRepMaxChartModel) {
+                "${LiftMetricChartType.ESTIMATED_ONE_REP_MAX.displayName()} " +
+                        if(oneRepMaxChartModel?.hasData != true) "- NO DATA" else ""
+            },
             oneRepMaxChartModel = oneRepMaxChartModel,
             workoutFilterOptions = workoutFilterOptions,
             selectedWorkoutFilters = selectedOneRepMaxWorkoutFilters,
             onApplyWorkoutFilters = onFilterOneRepMaxChartByWorkouts,
         )
         MultiLineWorkoutFilterableChart(
-            label = remember(volumeChartModel) { "VOLUME ${if(volumeChartModel?.hasData != true) "- NO DATA" else ""}" },
+            label = remember(volumeChartModel) { "${LiftMetricChartType.VOLUME.displayName()} " +
+                    if(volumeChartModel?.hasData != true) "- NO DATA" else ""
+            },
             volumeChartModel = volumeChartModel,
             workoutFilterOptions = workoutFilterOptions,
             selectedWorkoutFilters = selectedVolumeWorkoutFilters,
             onApplyWorkoutFilters = onFilterVolumeChartByWorkouts,
         )
         SingleLineWorkoutFilterableChart(
-            label = remember(oneRepMaxChartModel) { "RELATIVE INTENSITY ${if(oneRepMaxChartModel?.hasData != true) "- NO DATA" else ""}" },
+            label = remember(oneRepMaxChartModel) { "${LiftMetricChartType.RELATIVE_INTENSITY.displayName()} " +
+                    if(oneRepMaxChartModel?.hasData != true) "- NO DATA" else ""
+            },
             oneRepMaxChartModel = intensityChartModel,
             workoutFilterOptions = workoutFilterOptions,
             selectedWorkoutFilters = selectedIntensityWorkoutFilters,
