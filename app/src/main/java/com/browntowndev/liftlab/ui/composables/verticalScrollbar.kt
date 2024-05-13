8cd94e8a9c16b383a6134bdf7dd794fd9236c3f0
Sean Brown <seanbrown91@gmx.com>
2024-05-13 12:26:33 -0400

    Add scrollbar for lift library

---
+++ app/src/main/java/com/browntowndev/liftlab/ui/composables/verticalScrollbar.kt
@@ -0,0 +1,171 @@
+package com.browntowndev.liftlab.ui.composables
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.unit.dp
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+@Composable
+fun Modifier.verticalScrollbar(
+    lazyListState: LazyListState,
+    color: Color = MaterialTheme.colorScheme.outline,
+    onRequestLocationIdentifier: (index: Int) -> String,
+): Modifier {
+    val handleWidth = 3.dp
+    val targetAlpha = if (lazyListState.isScrollInProgress) 1f else 0f
+    val duration = if (lazyListState.isScrollInProgress) 150 else 500
+    val alpha by animateFloatAsState(
+        targetValue = targetAlpha,
+        animationSpec = tween(durationMillis = duration),
+        label = "Scrollbar handle animation"
+    )
+    val colorWithAlphaUpdated = remember(alpha) { color.copy(alpha = alpha)}
+
+    var elementHeight by remember { mutableFloatStateOf(0f) }
+    val firstVisibleElementIndex =
+        remember { derivedStateOf { lazyListState.layoutInfo } }.value.visibleItemsInfo.firstOrNull()?.index
+            ?: 0
+    val needDrawScrollbar = lazyListState.isScrollInProgress || alpha > 0.0f
+    val scrollbarOffsetY by animateFloatAsState(
+        targetValue = firstVisibleElementIndex * elementHeight,
+        animationSpec = tween(durationMillis = 100),
+        label = "Scrollbar offset animation"
+    )
+    var isHandleHeightSet by remember { mutableStateOf(false) }
+    var handleHeight by remember { mutableFloatStateOf(0f) }
+
+    return drawWithContent {
+        drawContent()
+
+        if (needDrawScrollbar) {
+            elementHeight = this.size.height / lazyListState.layoutInfo.totalItemsCount
+            handleHeight =
+                if (!isHandleHeightSet) lazyListState.layoutInfo.visibleItemsInfo.size * elementHeight else handleHeight
+            isHandleHeightSet = true
+
+            drawRoundRect(
+                color = colorWithAlphaUpdated,
+                cornerRadius = CornerRadius(10f, 10f),
+                topLeft = Offset(this.size.width - handleWidth.toPx(), scrollbarOffsetY),
+                size = Size(handleWidth.toPx(), handleHeight),
+            )
+
+            // Calculate bubble circle size
+            val locationIdentifier = onRequestLocationIdentifier(firstVisibleElementIndex)
+            val bubbleRadius = handleWidth.toPx() * 6f
+            val bubbleCenter = Offset(
+                this.size.width - handleWidth.toPx() * 9f,
+                scrollbarOffsetY + handleHeight / 2
+            )
+
+            // Draw the bubble
+            val bubblePath = Path().apply {
+                // Add the circle to the path
+                addOval(Rect(center = bubbleCenter, radius = bubbleRadius))
+
+                // Calculate the intersection points between the triangle lines and the circle
+                val triangleSize = handleWidth.toPx() * 2f
+                val triangleBasePoint = Offset(bubbleCenter.x + bubbleRadius + triangleSize, bubbleCenter.y)
+                val triangleTopPoint = Offset(bubbleCenter.x, bubbleCenter.y + bubbleRadius)
+                val triangleBottomPoint = Offset(bubbleCenter.x, bubbleCenter.y - bubbleRadius)
+
+                val topIntersectionPoint = calculateLineCircleIntersection(triangleBasePoint, triangleTopPoint, bubbleCenter, bubbleRadius)
+                val bottomIntersectionPoint = calculateLineCircleIntersection(triangleBasePoint, triangleBottomPoint, bubbleCenter, bubbleRadius)
+
+                // Draw the triangle path
+                moveTo(triangleBasePoint.x, triangleBasePoint.y)
+                lineTo(topIntersectionPoint.x, topIntersectionPoint.y)
+                val startAngleDegrees = atan2(topIntersectionPoint.y - bubbleCenter.y, topIntersectionPoint.x - bubbleCenter.x)
+                arcTo(
+                    rect = Rect(bubbleCenter.x - bubbleRadius, bubbleCenter.y - bubbleRadius, bubbleCenter.x + bubbleRadius, bubbleCenter.y + bubbleRadius),
+                    startAngleDegrees = startAngleDegrees,
+                    sweepAngleDegrees = atan2(bottomIntersectionPoint.y - bubbleCenter.y, bottomIntersectionPoint.x - bubbleCenter.x) - startAngleDegrees,
+                    forceMoveTo = false
+                )
+                lineTo(bottomIntersectionPoint.x, bottomIntersectionPoint.y)
+                close()
+            }
+
+            drawPath(
+                path = bubblePath,
+                color = colorWithAlphaUpdated,
+            )
+
+            // Draw the text inside the bubble
+            val textPaint = android.graphics.Paint().apply {
+                textSize = bubbleRadius
+                setColor(invertColor(colorWithAlphaUpdated).value.toLong())
+            }
+            drawIntoCanvas { canvas ->
+                canvas.nativeCanvas.drawText(
+                    locationIdentifier,
+                    bubbleCenter.x - textPaint.measureText(locationIdentifier) / 2,
+                    bubbleCenter.y + (textPaint.textSize * .8f) / 2,
+                    textPaint
+                )
+            }
+        }
+    }
+}
+
+private fun invertColor(color: Color): Color {
+    val red = (255 - color.red * 255) / 255
+    val green = (255 - color.green * 255) / 255
+    val blue = (255 - color.blue * 255) / 255
+
+    return Color(
+        red = red,
+        green = green,
+        blue = blue,
+        alpha = color.alpha,
+        colorSpace = color.colorSpace)
+}
+
+private fun calculateLineCircleIntersection(
+    lineStart: Offset,
+    lineEnd: Offset,
+    circleCenter: Offset,
+    circleRadius: Float
+): Offset {
+    val dx = lineEnd.x - lineStart.x
+    val dy = lineEnd.y - lineStart.y
+    val a = dx * dx + dy * dy
+    val b = 2 * (dx * (lineStart.x - circleCenter.x) + dy * (lineStart.y - circleCenter.y))
+    val c = (lineStart.x - circleCenter.x) * (lineStart.x - circleCenter.x) +
+            (lineStart.y - circleCenter.y) * (lineStart.y - circleCenter.y) -
+            circleRadius * circleRadius
+
+    val discriminant = b * b - 4 * a * c
+    if (discriminant < 0) {
+        // No intersection
+        return lineEnd
+    }
+
+    val t = (-b - sqrt(discriminant.toDouble())) / (2 * a)
+    val intersectionX = lineStart.x + t * dx
+    val intersectionY = lineStart.y + t * dy
+
+    return Offset(intersectionX.toFloat(), intersectionY.toFloat())
+}
